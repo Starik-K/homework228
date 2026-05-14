9550452c46b2ecd718934ffdfbdc8dd4a5a55c14
Starik-K <levsl777@gmail.com>
2026-05-14 19:42:41 +0300

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,157 @@
+//задание 1
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int n;
+//
+//    cin >> n;
+//
+//    if (n % 2 == 0)
+//        cout << "Парне";
+//    else
+//        cout << "Непарне";
+//
+//    return 0;
+//}
+//
+//задание 2
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int a, b;
+//
+//    cin >> a >> b;
+//
+//    if (a < b)
+//        cout << a;
+//    else
+//        cout << b;
+//
+//    return 0;
+//}
+//
+//задание 3
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int n;
+//
+//    cin >> n;
+//
+//    if (n > 0)
+//        cout << "Додатне";
+//    else if (n < 0)
+//        cout << "Від'ємне";
+//    else
+//        cout << "Нуль";
+//
+//    return 0;
+//}
+//
+//задание 4
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int a, b;
+//
+//    cin >> a >> b;
+//
+//    if (a == b)
+//        cout << "Рівні";
+//    else {
+//        if (a < b)
+//            cout << a << " " << b;
+//        else
+//            cout << b << " " << a;
+//    }
+//
+//    return 0;
+//}
+//
+//задание 5
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int a, b, c, d, e;
+//    double s;
+//
+//    cin >> a >> b >> c >> d >> e;
+//
+//    s = (a + b + c + d + e) / 5.0;
+//
+//    if (s >= 4)
+//        cout << "Допущено";
+//    else
+//        cout << "Не допущено";
+//
+//    return 0;
+//}
+//
+//задание 6
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    double n;
+//
+//    cin >> n;
+//
+//    if ((int)n % 2 == 0)
+//        cout << n * 3;
+//    else
+//        cout << n / 2;
+//
+//    return 0;
+//}
+//
+//задание 8
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int n, p;
+//    int r = 1;
+//
+//    cin >> n >> p;
+//
+//    if (p >= 0 && p <= 7) {
+//        for (int i = 0; i < p; i++) {
+//            r *= n;
+//        }
+//
+//        cout << r;
+//    }
+//
+//    return 0;
+//}
+//задание 7
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    double a, b;
+//    char op;
+//
+//    cin >> a >> b >> op;
+//
+//    if (op == '+')
+//        cout << a + b;
+//
+//    if (op == '-')
+//        cout << a - b;
+//
+//    if (op == '*')
+//        cout << a * b;
+//
+//    if (op == '/')
+//        cout << a / b;
+//
+//    return 0;
+//}
+//
